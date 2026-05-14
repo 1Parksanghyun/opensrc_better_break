6b0fa432a7a6efc0d9eb273abe981610fa6b6c56
1Parksanghyun <nks168810@gmail.com>
2026-05-14 09:56:40 +0900

    test and setting

--- app/src/main/java/com/example/opensrc_better_break/MainActivity.kt
+++ app/src/main/java/com/example/opensrc_better_break/MainActivity.kt
@@ -12,11 +12,34 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.opensrc_better_break.ui.theme.Opensrc_better_breakTheme
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val db = FirebaseFirestore.getInstance()
+
+        val testSensorData = hashMapOf(
+            "location" to "테스트 구역",
+            "temperature" to 22.5,
+            "humidity" to 55.0,
+            "message" to "파이어베이스 연결 성공!"
+        )
+
+        db.collection("test_sensors")
+            .add(testSensorData)
+            .addOnSuccessListener { documentReference ->
+                // 성공하면 하단 Logcat에 초록색으로 출력됨
+                Log.d("FirebaseTest", "데이터 저장 성공! 문서 ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                // 실패하면 빨간색으로 출력됨
+                Log.w("FirebaseTest", "데이터 저장 실패...", e)
+            }
+
         setContent {
             Opensrc_better_breakTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
